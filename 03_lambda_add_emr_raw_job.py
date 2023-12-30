3ee8419e19e54a11242b570653d25003b53130e5
EDUARDO LIMA <weduardow123@gmail.com>
2023-12-30 11:56:09 -0300

    read transfer control and find related spark job

--- 03_lambda_add_emr_raw_job.py
+++ 03_lambda_add_emr_raw_job.py
@@ -1,8 +1,19 @@
 import boto3 
+import os
+import pandas as pd
+from io import StringIO
+import re
 
 def get_emr_client():
     client = boto3.client(
         'emr',
+        region_name = 'us-east-1'
+        )
+    return client
+
+def get_s3_client():
+    client = boto3.client(
+        's3',
         region_name = 'us-east-1'
         )
     return client
@@ -15,7 +26,7 @@ def get_emr_list(emr_client):
             return cluster
     return None
 
-def add_emr_step(cluster_id, s3_script_path):
+def add_spark_job_to_emr(cluster_id, s3_script_path):
     emr_client = boto3.client('emr', region_name='us-east-1')
 
     step = [
@@ -36,9 +47,30 @@ def add_emr_step(cluster_id, s3_script_path):
 
     print('Step job added')
     return response
+
+def find_spark_job(s3_client, file_name):
+    BUCKET_CONTROL_NAME = os.getenv('AWS_BUCKET_CONTROL_NAME')
+    TRANSFER_CONTROL_S3_KEY = 'transfer_control.csv'
+
+    response = s3_client.get_object(Bucket=BUCKET_CONTROL_NAME, Key=TRANSFER_CONTROL_S3_KEY)
+    csv_content = response['Body'].read().decode('utf-8')
+
+    df = pd.read_csv(StringIO(csv_content))
+
+    search_regex = re.escape(file_name)
+    print(search_regex)
+
+    for index, row in df.iterrows():
+        if re.search(row['regex_file_name'], search_regex, re.IGNORECASE):
+           return row['spark_job_s3_key']
+    else:
+        raise Exception(f'Spark job was not found inside transfer control. Key {TRANSFER_CONTROL_S3_KEY}')
     
+file_name = "olist_customers_dataset_20231214170632.csv"
 
 emr_client = get_emr_client()
+s3_client = get_s3_client()
 emr_ecommerce_cluster = get_emr_list(emr_client)
+spark_job_s3_path = find_spark_job(s3_client, file_name)
 
-add_emr_step(emr_ecommerce_cluster['Id'], 's3://ecommerce-project-control/jupyter/jovyan/raw/0001_raw_customers.py')
+add_spark_job_to_emr(emr_ecommerce_cluster['Id'], spark_job_s3_path)
