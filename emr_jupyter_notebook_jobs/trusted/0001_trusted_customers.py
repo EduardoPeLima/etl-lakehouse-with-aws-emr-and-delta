bbbb9d7ae95af69c4f9f44c26f69e14315a602db
EDUARDO LIMA <weduardow123@gmail.com>
2024-01-08 10:22:00 -0300

    trusted customers scd2 tested and log developed

--- emr_jupyter_notebook_jobs/trusted/0001_trusted_customers.py
+++ emr_jupyter_notebook_jobs/trusted/0001_trusted_customers.py
@@ -1,7 +1,7 @@
-import re
 from pyspark.sql import SparkSession, SQLContext
 from pyspark.sql.functions import lit, col
 from datetime import datetime, timedelta
+import re
 
 spark = SparkSession.builder \
     .appName('0001_raw_customers') \
@@ -18,18 +18,7 @@ spark = SparkSession.builder \
 spark.sparkContext.addPyFile("s3://spark-addons/"\
                              +"delta-core_2.12-2.4.0.jar")
 
-from delta.tables import *
-
-#sqlContext=SQLContext(spark.sparkContext)
-
-%%configure -f
-{
-  "conf": {
-    "spark.sql.extensions": "io.delta.sql.DeltaSparkSessionExtension",
-    "spark.sql.catalog.spark_catalog": "org.apache.spark.sql.delta.catalog.DeltaCatalog"
-    "spark.jars.packages": "io.delta:delta-storage-2.4.0"
-  }
-}
+from delta import *
 
 #prefix list
 
@@ -51,72 +40,20 @@ str_s3_raw_file_path = f's3://{str_bucket_raw}/{key_file_path}'
 print(str_s3_raw_file_path)
 
 raw_customers = spark.read.format("delta").load(str_s3_raw_file_path)
-raw_customers = raw_customers.withColumn('join_key',lit(""))
 
 raw_customers.createOrReplaceTempView("raw_customers")
 
-raw_customers.cache()
-qtd=raw_customers.count()
-print('rows from landzone file: ', qtd)
+#raw_customers.cache()
+#qtd=raw_customers.count()
+#print('rows from landzone file: ', qtd)
 
 #SCD2 will be utilized, and must happen a new register for a customer when his 
 #customer_zip_code_prefix, customer_city or customer_state has changed.
 
-##célula temporária para simular que a raw_customers tem uma alteração de cidade
-
-raw_customers = spark.sql(
-"""
-    SELECT
-        ref_day,
-        ref_day_partition,
-        ref_file_extraction,
-        ref_file_extraction_partition,
-        customer_id,
-        customer_unique_id,
-        customer_zip_code_prefix,
-        case
-            when customer_id = "503840d4f2a1a7609f6489f44ffa9f7c" then "teste2"
-            else customer_city
-        end as customer_city,
-        customer_state
-    FROM raw_customers
-"""
-)
-
-raw_customers.write\
-    .partitionBy('ref_day_partition','ref_file_extraction_partition') \
-    .format("delta") \
-    .mode("overwrite") \
-    .save(str_s3_raw_file_path)
-
 str_s3_trusted_file_path = f's3://{str_bucket_trusted}/{key_file_path}'
 print(str_s3_trusted_file_path)
 
-spark.sql(
-"""
-select 
-    trusted.*,
-    raw.customer_id as rola,
-    raw.customer_city as teste
-from delta.`s3://ecommerce-project-trusted/ecommerce/olist_customers_dataset` as trusted
-left join delta.`s3://ecommerce-project-raw/ecommerce/olist_customers_dataset` as raw
-on raw.customer_id = trusted.customer_id
-where trusted.customer_id = "503840d4f2a1a7609f6489f44ffa9f7c"
-"""
-).show(5)
-
-+--------+-------------------+--------------------+--------------------+------------------------+-------------+--------------+--------------------+--------------------+---------------+--------------------+------+
-| ref_day|ref_file_extraction|         customer_id|  customer_unique_id|customer_zip_code_prefix|customer_city|customer_state|       ts_start_date|         ts_end_date|flag_scd_active|                rola| teste|
-+--------+-------------------+--------------------+--------------------+------------------------+-------------+--------------+--------------------+--------------------+---------------+--------------------+------+
-|20240104|               null|503840d4f2a1a7609...|ffc4233210eac4ec1...|                   14811|   araraquara|            SP|2024-01-05 02:15:...|2024-01-06 17:33:...|          false|503840d4f2a1a7609...|teste2|
-|20240104|               null|503840d4f2a1a7609...|ffc4233210eac4ec1...|                   14811|        teste|            SP|2024-01-06 17:06:...|2024-01-06 17:33:...|          false|503840d4f2a1a7609...|teste2|
-|20240104|               null|503840d4f2a1a7609...|ffc4233210eac4ec1...|                   14811|       teste2|            SP|2024-01-06 17:33:...|                null|           true|503840d4f2a1a7609...|teste2|
-+--------+-------------------+--------------------+--------------------+------------------------+-------------+--------------+--------------------+--------------------+---------------+--------------------+------+
-
-trusted_customers_now = DeltaTable.forPath(
-        spark, str_s3_trusted_file_path)
-
-spark.sql(
+merge = spark.sql(
 """
     MERGE INTO delta.`s3://ecommerce-project-trusted/ecommerce/olist_customers_dataset` as trusted
     USING
@@ -153,6 +90,7 @@ spark.sql(
             AND trusted.flag_scd_active = True      
     ) as sub
     ON sub.join_key = trusted.customer_id
+    AND trusted.flag_scd_active = True
     WHEN MATCHED
     AND (
         sub.customer_zip_code_prefix <> trusted.customer_zip_code_prefix 
@@ -191,51 +129,62 @@ spark.sql(
 """
 )
 
+merge.createOrReplaceTempView("merge")
 
+print("merge completed from raw to trusted")
 
-
-
-############ZONA PARA TESTAR O SCD 2
-
-#celula temporaria para criar primeira trusted customers no S3
-
-trusted_customers = spark.sql(
-"""
-    SELECT
-        ref_day,
-        ref_file_extraction,
-        customer_id,
-        customer_unique_id,
-        customer_zip_code_prefix,
-        customer_city,
-        customer_state,
-        current_timestamp() as ts_start_date,
-        CAST(NULL AS TIMESTAMP) as ts_end_date,
-        True as flag_scd_active
-    FROM raw_customers
-"""
+control = spark.sql(
+    f"""
+        SELECT
+            "{str_bucket_raw}" as str_origin_zone,
+            "{str_bucket_trusted}" as str_target_zone,
+            '0001_trusted_customers' as str_process, 
+            "{str_s3_raw_file_path}" as str_origin_file_path,
+            "{str_s3_trusted_file_path}" as str_target_file_path,
+            int("{str_proc_brazilian_datetime}") as dt_proc,
+            num_affected_rows,
+            num_updated_rows,
+            num_deleted_rows,
+            num_inserted_rows
+        FROM merge
+    """
 )
 
-trusted_customers.show(5)
+str_control_path = f's3://{str_bucket_control}/tb_0001_control_process_trusted'
+print(str_control_path)
 
-trusted_customers.write \
-    .mode('overwrite') \
-    .format('delta').save(str_s3_trusted_file_path)
+control.write \
+    .format('delta') \
+    .mode('append') \
+    .save(str_control_path)
 
-df_teste_scd2 = spark.read.format('delta').load(str_s3_trusted_file_path)
+print('Log appended to control')
 
-df_teste_scd2.show(5, truncate=False)
+#temp code to test scd 2 in trusted
 
-+--------+-------------------+--------------------------------+--------------------------------+------------------------+---------------------+--------------+-----------------------+-----------+---------------+
-|ref_day |ref_file_extraction|customer_id                     |customer_unique_id              |customer_zip_code_prefix|customer_city        |customer_state|ts_start_date          |ts_end_date|flag_scd_active|
-+--------+-------------------+--------------------------------+--------------------------------+------------------------+---------------------+--------------+-----------------------+-----------+---------------+
-|20240104|null               |06b8999e2fba1a1fbc88172c00ba8bc7|861eff4711a542e4b93843c6dd7febb0|14409                   |franca               |SP            |2024-01-05 02:15:44.496|null       |true           |
-|20240104|null               |18955e83d337fd6b2def6b18a428ac77|290c77bc529b7ac935b93aa66c333dc3|09790                   |sao bernardo do campo|SP            |2024-01-05 02:15:44.496|null       |true           |
-|20240104|null               |4e7b3e00288586ebd08712fdd0374a03|060e732b5b29e8181a18229c7b0b2b5e|01151                   |sao paulo            |SP            |2024-01-05 02:15:44.496|null       |true           |
-|20240104|null               |b2b6027bc5c5109e529d4dc6358b12c3|259dac757896d24d7702b9acbbff3f3c|08775                   |mogi das cruzes      |SP            |2024-01-05 02:15:44.496|null       |true           |
-|20240104|null               |4f2d8ab171c80ec8364f7c12e35b23ad|345ecd01c38d18a9036ed96c73b8d066|13056                   |campinas             |SP            |2024-01-05 02:15:44.496|null       |true           |
-+--------+-------------------+--------------------------------+--------------------------------+------------------------+---------------------+--------------+-----------------------+-----------+---------------+
-
-###teste no customer_id "503840d4f2a1a7609f6489f44ffa9f7c"
-
-
+#raw_customers = spark.sql(
+#"""
+#    SELECT
+#        ref_day,
+#        ref_day_partition,
+#        ref_file_extraction,
+#        ref_file_extraction_partition,
+#        customer_id,
+#        customer_unique_id,
+#        customer_zip_code_prefix,
+#        case
+#            when customer_id = "503840d4f2a1a7609f6489f44ffa9f7c" then "teste3"
+#            else customer_city
+#        end as customer_city,
+#        customer_state
+#    FROM raw_customers
+#"""
+#)
+#
+#raw_customers.write\
+#    .partitionBy('ref_day_partition','ref_file_extraction_partition') \
+#    .format("delta") \
+#    .mode("overwrite") \
+#    .save(str_s3_raw_file_path)
+#
+#raw_customers.createOrReplaceTempView("raw_customers")
