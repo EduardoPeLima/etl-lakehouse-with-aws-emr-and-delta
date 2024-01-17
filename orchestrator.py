c21f8cb9efbfe53e975885c4e71b00ad1fad0367
EDUARDO LIMA <weduardow123@gmail.com>
2024-01-17 17:00:02 -0300

    added all trusted and gold jobs

--- orchestrator.py
+++ orchestrator.py
@@ -12,7 +12,7 @@ def main():
     EMR_CLIENT = get_aws_clients.get_emr_client()
 
     data_model = DataModel(S3_AWS_CLIENT=S3_CLIENT, INGESTION_BUCKET=INGESTION_BUCKET)
-    data_model.ingest_local_data_to_s3()
+    #data_model.ingest_local_data_to_s3()
 
     cluster_name = "emr_ecommerce_project"
     release_label = "emr-6.15.0"
@@ -34,10 +34,18 @@ def main():
     #trusted
     submit_transform_jobs_to_emr.add_spark_job_to_emr('s3://ecommerce-project-control/jupyter/jovyan/trusted/0001_trusted_customers.py')
     submit_transform_jobs_to_emr.add_spark_job_to_emr('s3://ecommerce-project-control/jupyter/jovyan/trusted/0002_trusted_orders.py')
+    submit_transform_jobs_to_emr.add_spark_job_to_emr('s3://ecommerce-project-control/jupyter/jovyan/trusted/0003_trusted_order_items.py')
+    submit_transform_jobs_to_emr.add_spark_job_to_emr('s3://ecommerce-project-control/jupyter/jovyan/trusted/0004_trusted_order_payments.py')
+    submit_transform_jobs_to_emr.add_spark_job_to_emr('s3://ecommerce-project-control/jupyter/jovyan/trusted/0005_trusted_products.py')
 
-    #gold
-    
-
+    #gold: dimensional table and aggregate tables
+    submit_transform_jobs_to_emr.add_spark_job_to_emr('s3://ecommerce-project-control/jupyter/jovyan/refined/0001_dw_dim_products.py')
+    submit_transform_jobs_to_emr.add_spark_job_to_emr('s3://ecommerce-project-control/jupyter/jovyan/refined/0002_dw_dim_location.py')
+    submit_transform_jobs_to_emr.add_spark_job_to_emr('s3://ecommerce-project-control/jupyter/jovyan/refined/0003_dw_dim_customers.py')
+    #the fact should be the last dimensional model table processed, because it uses SK (Surrogate Key) from customers, and in the future new SK's can show up. 
+    submit_transform_jobs_to_emr.add_spark_job_to_emr('s3://ecommerce-project-control/jupyter/jovyan/refined/0004_dw_fact_orders.py')
+    submit_transform_jobs_to_emr.add_spark_job_to_emr('s3://ecommerce-project-control/jupyter/jovyan/refined/0005_aggregate_sales_performance_by_city.py')
+    submit_transform_jobs_to_emr.add_spark_job_to_emr('s3://ecommerce-project-control/jupyter/jovyan/refined/0006_aggregate_sales_per_month.py')
 
 if __name__ == "__main__":
     main()
